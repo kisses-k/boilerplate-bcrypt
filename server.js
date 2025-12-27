d40b8cfe3d3355b4c695687a0443ef6f808c3596
kisses-k <kamilahvelante@gmail.com>
2025-12-27 20:11:13 +0800

    Refactor password hashing and update logging

--- server.js
+++ server.js
@@ -1,19 +1,13 @@
 'use strict';
 const bcrypt = require('bcrypt');
-const myPlaintextPassword = "password123";
-const saltRounds = 12;
-
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  console.log(hash);
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-    console.log(res);
-  });
-});
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 const app         = express();
+
 fccTesting(app);
+
+// These variables were already provided by the boilerplate
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
@@ -21,7 +15,12 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 //END_ASYNC
 
@@ -31,35 +30,7 @@ const someOtherPlaintextPassword = 'pass123';
 
 //END_SYNC
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+  console.log("Listening on port:", PORT)
 });
